d1df6201e79a55334c859dc7d539574f13c583cd
AnhND <anh@gmail.com>
2026-06-29 16:05:21 +0700

    Update

--- run_gptaq.sh
+++ run_gptaq.sh
@@ -1,5 +1,14 @@
+# PYTHONPATH=. python eigenflip/run_gptaq.py \
+#   --model-path /home/DATA/prometheus/anh/.cache/huggingface/hub/models--Qwen--Qwen2.5-7B/snapshots/d149729398750b98c0af14eb82c78cfe92750796  --output-dir ./quantized_models/gptaq_3bit \
+#   --bits 3 --group-size 128 --asym-alpha 0.25 \
+#   --calib-dataset c4 --n-calib 128 --seqlen 2048 \
+#   --eval --eval-datasets wikitext2 c4 --eval-seqlen 2048 \
+#   --delete-after-eval
+
+
 PYTHONPATH=. python eigenflip/run_gptaq.py \
-  --model-path /home/DATA/prometheus/anh/.cache/huggingface/hub/models--Qwen--Qwen2.5-7B/snapshots/d149729398750b98c0af14eb82c78cfe92750796  --output-dir ./quantized_models/gptaq_3bit \
+  --model-path /home/DATA/prometheus/anh/.cache/huggingface/hub/models--mistralai--Mistral-7B-v0.3/snapshots/caa1feb0e54d415e2df31207e5f4e273e33509b1
+  --output-dir ./quantized_models/gptaq_mistral_3bit \
   --bits 3 --group-size 128 --asym-alpha 0.25 \
   --calib-dataset c4 --n-calib 128 --seqlen 2048 \
   --eval --eval-datasets wikitext2 c4 --eval-seqlen 2048 \
