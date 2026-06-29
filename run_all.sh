f6efd86a8fe3a99077a1c794da1ab34c3dcbd315
AnhND <anh@gmail.com>
2026-06-29 16:09:17 +0700

    Update

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
-MODEL_PATH=/home/DATA/prometheus/anh/.cache/huggingface/hub/models--mistralai--Mistral-7B-v0.3/snapshots/caa1feb0e54d415e2df31207e5f4e273e33509b1  #
-#MODEL_PATH=/home/DATA/prometheus/anh/.cache/huggingface/hub/models--meta-llama--Meta-Llama-3.1-8B/snapshots/d04e592bb4f6aa9cfee91e2e20afa771667e1d4b
+#MODEL_PATH=/home/DATA/prometheus/anh/.cache/huggingface/hub/models--mistralai--Mistral-7B-v0.3/snapshots/caa1feb0e54d415e2df31207e5f4e273e33509b1  #
+MODEL_PATH=/home/DATA/prometheus/anh/.cache/huggingface/hub/models--meta-llama--Meta-Llama-3.1-8B/snapshots/d04e592bb4f6aa9cfee91e2e20afa771667e1d4b
 #MODEL_PATH=/home/DATA/prometheus/anh/.cache/huggingface/hub/models--Qwen--Qwen2.5-7B/snapshots/d149729398750b98c0af14eb82c78cfe92750796 
-OUTPUT_DIR=./quantized_models/eigenflip_mistral_3bit
+OUTPUT_DIR=./quantized_models/eigenflip_llama_3bit
 LOG_DIR=./logs
 mkdir -p "$LOG_DIR"
 LOG_FILE="$LOG_DIR/eigenflip_3bit_$(date +%Y%m%d_%H%M%S).log"
