052a984d8abb066013eb9aadd2b58f403e9805b5
AnhND <anh@gmail.com>
2026-06-30 07:41:03 +0700

    Update

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
-MODEL_PATH=/home/DATA/prometheus/anh/.cache/huggingface/hub/models--mistralai--Mistral-7B-v0.3/snapshots/caa1feb0e54d415e2df31207e5f4e273e33509b1  #
+MODEL_PATH=/home/DATA/prometheus/anh/.cache/huggingface/hub/models--meta-llama--Meta-Llama-3-8B/snapshots/8cde5ca8380496c9a6cc7ef3a8b46a0372a1d920
+#MODEL_PATH=/home/DATA/prometheus/anh/.cache/huggingface/hub/models--mistralai--Mistral-7B-v0.3/snapshots/caa1feb0e54d415e2df31207e5f4e273e33509b1  #
 #MODEL_PATH=/home/DATA/prometheus/anh/.cache/huggingface/hub/models--meta-llama--Meta-Llama-3.1-8B/snapshots/d04e592bb4f6aa9cfee91e2e20afa771667e1d4b
 #MODEL_PATH=/home/DATA/prometheus/anh/.cache/huggingface/hub/models--Qwen--Qwen2.5-7B/snapshots/d149729398750b98c0af14eb82c78cfe92750796 
-OUTPUT_DIR=./quantized_models/eigenflip_llama_3bit
+OUTPUT_DIR=./quantized_models/eigenflip_llama3_3bit
 LOG_DIR=./logs
 mkdir -p "$LOG_DIR"
 LOG_FILE="$LOG_DIR/eigenflip_3bit_$(date +%Y%m%d_%H%M%S).log"
--- run_gptaq.sh
+++ run_gptaq.sh
@@ -6,13 +6,13 @@
 #   --delete-after-eval
 
 
-PYTHONPATH=. python eigenflip/run_gptaq.py \
-  --model-path /home/DATA/prometheus/anh/.cache/huggingface/hub/models--mistralai--Mistral-7B-v0.3/snapshots/caa1feb0e54d415e2df31207e5f4e273e33509b1 \
-  --output-dir ./quantized_models/gptaq_mistral_3bit \
-  --bits 3 --group-size 128 --asym-alpha 0.25 \
-  --calib-dataset c4 --n-calib 128 --seqlen 2048 \
-  --eval --eval-datasets wikitext2 c4 --eval-seqlen 2048 \
-  --delete-after-eval
+# PYTHONPATH=. python eigenflip/run_gptaq.py \
+#   --model-path /home/DATA/prometheus/anh/.cache/huggingface/hub/models--mistralai--Mistral-7B-v0.3/snapshots/caa1feb0e54d415e2df31207e5f4e273e33509b1 \
+#   --output-dir ./quantized_models/gptaq_mistral_3bit \
+#   --bits 3 --group-size 128 --asym-alpha 0.25 \
+#   --calib-dataset c4 --n-calib 128 --seqlen 2048 \
+#   --eval --eval-datasets wikitext2 c4 --eval-seqlen 2048 \
+#   --delete-after-eval
 
 # PYTHONPATH=. python eigenflip/run_gptaq.py \
 #   --model-path /home/DATA/prometheus/anh/.cache/huggingface/hub/models--meta-llama--Meta-Llama-3.1-8B/snapshots/d04e592bb4f6aa9cfee91e2e20afa771667e1d4b \
@@ -21,3 +21,11 @@ PYTHONPATH=. python eigenflip/run_gptaq.py \
 #   --calib-dataset c4 --n-calib 128 --seqlen 2048 \
 #   --eval --eval-datasets wikitext2 c4 --eval-seqlen 2048 \
 #   --delete-after-eval
+
+PYTHONPATH=. python eigenflip/run_gptaq.py \
+  --model-path /home/DATA/prometheus/anh/.cache/huggingface/hub/models--meta-llama--Meta-Llama-3-8B/snapshots/8cde5ca8380496c9a6cc7ef3a8b46a0372a1d920 \
+  --output-dir ./quantized_models/gptaq_llama3_3bit \
+  --bits 3 --group-size 128 --asym-alpha 0.25 \
+  --calib-dataset c4 --n-calib 128 --seqlen 2048 \
+  --eval --eval-datasets wikitext2 c4 --eval-seqlen 2048 \
+  --delete-after-eval
